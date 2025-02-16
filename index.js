3ab9c6b85db72af181569b514d5abb7edb824638
Soulhunt25 <0105cs221134@oriemtal.ac.in>
2025-02-16 18:51:52 +0530

    code has been added by me

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-function checkprime(num){
-
+function checkprime(number){
+let factor = 0;
+for(let i=2;i<=number;i++){
+    if(number%i==0){
+        factor++;
+    }
 }
-
+if(factor==1){
+    console.log("Prime Number");
+}else{
+    console.log("Not a Prime Number");
+}
+}
 
 checkprime(5);
